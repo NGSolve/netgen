80536a3308532be36251775b8137893fcd1f18d1
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2016-12-22 19:12:11 +0100

    CMake: Add External projects to fetch and install some dependencies automatically on Windows and MacOS

---
+++ cmake_modules/ExternalProject_TCLTK.cmake
@@ -0,0 +1,69 @@
+if(APPLE)
+  set(HOME $ENV{HOME})
+  ExternalProject_Add(tcl
+    URL "http://sourceforge.net/projects/tcl/files/Tcl/8.6.4/tcl8.6.4-src.tar.gz"
+    UPDATE_COMMAND "" # Disable update
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make -C macosx install INSTALL_ROOT=${HOME}/ NATIVE_TCLSH=${HOME}/usr/local/bin/tclsh
+    INSTALL_COMMAND ""
+    LOG_DOWNLOAD 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+    )
+
+  ExternalProject_Add(tk
+    DEPENDS tcl
+    URL "http://sourceforge.net/projects/tcl/files/Tcl/8.6.4/tk8.6.4-src.tar.gz"
+    UPDATE_COMMAND "" # Disable update
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND make -C macosx install INSTALL_ROOT=${HOME}/
+    INSTALL_COMMAND ""#make -C macosx install
+    LOG_DOWNLOAD 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+    )
+
+  ExternalProject_Add(tkdnd
+    DEPENDS tcl tk
+    URL "http://sourceforge.net/projects/tkdnd/files/TkDND/TkDND%202.8/tkdnd2.8-src.tar.gz"
+    PATCH_COMMAND  patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/tkdnd_macosx.patch
+    UPDATE_COMMAND "" # Disable update
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ./configure --libdir=${HOME}/Library/Tcl --with-tcl=${HOME}/Library/Frameworks/Tcl.framework --with-tk=${HOME}/Library/Frameworks/Tk.framework --prefix=${HOME}
+    BUILD_COMMAND make
+    INSTALL_COMMAND make install
+    LOG_DOWNLOAD 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1
+    )
+
+  list(APPEND NETGEN_DEPENDENCIES tcl tk tkdnd)
+  set(TCL_INCLUDE_PATH ${HOME}/Library/Frameworks/Tcl.framework/Headers)
+  set(TCL_LIBRARY ${HOME}/Library/Frameworks/Tcl.framework)
+  set(TK_LIBRARY ${HOME}/Library/Frameworks/Tk.framework)
+  set(TK_INCLUDE_PATH ${HOME}/Library/Frameworks/Tk.framework/Headers)
+  set(TCL_TCLSH ${HOME}/usr/local/bin/tclsh)
+  set(TK_WISH ${HOME}/usr/local/bin/wish)
+
+elseif(WIN32)
+
+  ExternalProject_Add(win_extlibs
+    URL ${EXT_LIBS_DOWNLOAD_URL_WIN}
+    UPDATE_COMMAND "" # Disable update
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory . ${INSTALL_DIR}
+    LOG_DOWNLOAD 1
+    )
+
+  list(APPEND NETGEN_DEPENDENCIES win_extlibs)
+else(WIN32)
+    find_package(TCL 8.5 REQUIRED)
+endif(APPLE)
+
+# Propagate settings to Netgen subproject
+set_vars(NETGEN_CMAKE_ARGS TCL_INCLUDE_PATH TCL_LIBRARY TK_LIBRARY TK_INCLUDE_PATH TCL_TCLSH TK_WISH)
---
+++ cmake_modules/SuperBuild.cmake
@@ -0,0 +1,141 @@
+include (ExternalProject)
+
+set_property (DIRECTORY PROPERTY EP_PREFIX dependencies)
+
+set (NETGEN_DEPENDENCIES)
+set (LAPACK_DEPENDENCIES)
+set (NETGEN_CMAKE_ARGS)
+
+set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${PROJECT_SOURCE_DIR}/cmake_modules")
+
+macro(set_vars VAR_OUT)
+  foreach(varname ${ARGN})
+    if(NOT "${${varname}}" STREQUAL "")
+      string(REPLACE ";" "$<SEMICOLON>" varvalue "${${varname}}" )
+      list(APPEND ${VAR_OUT} -D${varname}=${varvalue})
+    endif()
+  endforeach()
+endmacro()
+#######################################################################
+if(WIN32)
+  if(NOT CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+    string(REGEX REPLACE "/W[0-4]" "/W0" CMAKE_CXX_FLAGS_NEW ${CMAKE_CXX_FLAGS})
+    set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS_NEW} CACHE STRING "compile flags" FORCE)
+    string(REGEX REPLACE "/W[0-4]" "/W0" CMAKE_CXX_FLAGS_NEW ${CMAKE_CXX_FLAGS_RELEASE})
+    set(CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_NEW} CACHE STRING "compile flags" FORCE)
+
+    string(REGEX REPLACE "/W[0-4]" "/W0" CMAKE_SHARED_LINKER_FLAGS_NEW ${CMAKE_SHARED_LINKER_FLAGS})
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS_NEW} /IGNORE:4217,4049" CACHE STRING "compile flags" FORCE)
+    string(REGEX REPLACE "/W[0-4]" "/W0" CMAKE_EXE_LINKER_FLAGS_NEW ${CMAKE_EXE_LINKER_FLAGS})
+    set(CMAKE_EXE_LINKER_FLAGS"${CMAKE_EXE_LINKER_FLAGS_NEW}/IGNORE:4217,4049" CACHE STRING "compile flags" FORCE)
+
+    set_vars(NETGEN_CMAKE_ARGS CMAKE_SHARED_LINKER_FLAGS CMAKE_SHARED_LINKER_FLAGS_RELEASE CMAKE_CXX_FLAGS CMAKE_CXX_FLAGS_RELEASE)
+  endif(NOT CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+
+  if(${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+    # 32 bit
+    set(EXT_LIBS_DOWNLOAD_URL_WIN "http://www.asc.tuwien.ac.at/~mhochsteger/ngsuite/ext_libs32.zip" CACHE STRING INTERNAL)
+    set(OCC_DOWNLOAD_URL_WIN "http://www.asc.tuwien.ac.at/~mhochsteger/ngsuite/occ32.zip" CACHE STRING INTERNAL)
+  else(${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+    # 64 bit
+    set(EXT_LIBS_DOWNLOAD_URL_WIN "http://www.asc.tuwien.ac.at/~mhochsteger/ngsuite/ext_libs64.zip" CACHE STRING INTERNAL)
+    set(OCC_DOWNLOAD_URL_WIN "http://www.asc.tuwien.ac.at/~mhochsteger/ngsuite/occ64.zip" CACHE STRING INTERNAL)
+  endif(${CMAKE_SIZEOF_VOID_P} MATCHES 4)
+endif(WIN32)
+
+#######################################################################
+# find netgen
+set(INSTALL_DIR /opt/netgen CACHE PATH "Install path")
+if(APPLE)
+  set(CMAKE_INSTALL_PREFIX "${INSTALL_DIR}/Netgen.app/Contents/Resources" CACHE INTERNAL "Prefix prepended to install directories" FORCE)
+else(APPLE)
+  set(CMAKE_INSTALL_PREFIX "${INSTALL_DIR}" CACHE INTERNAL "Prefix prepended to install directories" FORCE)
+endif(APPLE)
+
+if(NOT WIN32)
+    find_package(ZLIB REQUIRED)
+    set_vars(NETGEN_CMAKE_ARGS ZLIB_INCLUDE_DIRS ZLIB_LIBRARIES)
+endif(NOT WIN32)
+
+#######################################################################
+if (USE_PYTHON)
+    find_path(PYBIND_INCLUDE_DIR pybind11/pybind11.h ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies/pybind11/include)
+    if( NOT PYBIND_INCLUDE_DIR )
+      execute_process(COMMAND git submodule update --init --recursive WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+        find_path(PYBIND_INCLUDE_DIR pybind11/pybind11.h ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies/pybind11/include)
+    endif( NOT PYBIND_INCLUDE_DIR )
+    if( PYBIND_INCLUDE_DIR )
+        message("-- Found Pybind11: ${PYBIND_INCLUDE_DIR}")
+    else( PYBIND_INCLUDE_DIR )
+        message(FATAL_ERROR "Could NOT find pybind11!")
+    endif( PYBIND_INCLUDE_DIR )
+    set(CMAKE_MODULE_PATH "${CMAKE_MODULE_PATH}" "${PROJECT_SOURCE_DIR}/cmake_modules/python")
+    set(PYTHON_VERSION "3" CACHE STRING "Python version (only Python >= 3.0 supported)")
+    set(Python_ADDITIONAL_VERSIONS 3.5)
+    if( PYTHON_VERSION VERSION_LESS 3 )
+        message(FATAL_ERROR "Netgen supports only Python 3")
+    endif( PYTHON_VERSION VERSION_LESS 3 )
+    find_package(PythonInterp ${PYTHON_VERSION} REQUIRED)
+    find_package(PythonLibs ${PYTHON_VERSION}  REQUIRED)
+
+    set(PYTHON_LIBS "${PYTHON_LIBRARIES}")
+    execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib(1,0,''))" OUTPUT_VARIABLE PYTHON_PACKAGES_INSTALL_DIR OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set_vars(NETGEN_CMAKE_ARGS PYTHON_LIBS PYTHON_LIBRARY PYTHON_PACKAGES_INSTALL_DIR PYTHON_INCLUDE_DIR PYBIND_INCLUDE_DIR PYTHON_LIBRARIES PYTHON_EXECUTABLE PYTHON_VERSION)
+endif (USE_PYTHON)
+
+#######################################################################
+
+if(USE_OCC AND WIN32)
+    ExternalProject_Add(win_download_occ
+      PREFIX ${CMAKE_CURRENT_BINARY_DIR}/tcl
+      URL ${OCC_DOWNLOAD_URL_WIN}
+      UPDATE_COMMAND "" # Disable update
+      BUILD_IN_SOURCE 1
+      CONFIGURE_COMMAND ""
+      BUILD_COMMAND ""
+      INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory . ${INSTALL_DIR}
+      LOG_DOWNLOAD 1
+      )
+    list(APPEND NETGEN_DEPENDENCIES win_download_occ)
+endif(USE_OCC AND WIN32)
+
+#######################################################################
+
+if(USE_GUI)
+  include(cmake_modules/ExternalProject_TCLTK.cmake)
+endif(USE_GUI)
+
+#######################################################################
+# propagate cmake variables to Netgen subproject
+set_vars( NETGEN_CMAKE_ARGS
+  USE_GUI
+  USE_PYTHON
+  USE_MPI
+  USE_VT
+  USE_VTUNE
+  USE_NUMA
+  USE_CCACHE
+  USE_NATIVE_ARCH
+  USE_OCC
+  INSTALL_DIR
+  INSTALL_DEPENDENCIES
+  INTEL_MIC
+  )
+
+ExternalProject_Add (netgen
+  DEPENDS ${NETGEN_DEPENDENCIES}
+  SOURCE_DIR ${PROJECT_SOURCE_DIR}
+  CMAKE_ARGS -DUSE_SUPERBUILD=OFF ${NETGEN_CMAKE_ARGS} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}
+  INSTALL_COMMAND ""
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/netgen
+  BUILD_COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR}/netgen --config ${CMAKE_BUILD_TYPE}
+  STEP_TARGETS build
+)
+
+install(CODE "execute_process(COMMAND cmake --build . --target install --config ${CMAKE_BUILD_TYPE} WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/netgen)")
+
+add_custom_target(test_netgen
+  ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_BINARY_DIR}/netgen
+                   --target test
+                   --config ${CMAKE_BUILD_TYPE}
+                   )
