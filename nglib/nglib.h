d9f82838a389b34a81e03547b2c10ae9d026e6ed
Trevor Laughlin <trevor@laughlinresearch.com>
2017-10-30 21:34:25 -0400

    Manually apply netgen53ForSalome.patch

--- nglib/nglib.h
+++ nglib/nglib.h
@@ -24,7 +24,7 @@
 // Philippose - 14.02.2009
 // Modifications for creating a DLL in Windows
 #ifdef WIN32
-   #ifdef NGLIB_EXPORTS || nglib_EXPORTS
+   #if defined NGLIB_EXPORTS || defined nglib_EXPORTS
       #define DLL_HEADER   __declspec(dllexport)
    #else
       #define DLL_HEADER   __declspec(dllimport)
