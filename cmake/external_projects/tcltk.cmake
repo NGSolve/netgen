ed494caa443f066a1f590a9a40f0916f07815b96
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-11-30 13:40:30 +0100

    [cmake] Build tkdnd with CMake and correct Tcl/Tk version

--- cmake/external_projects/tcltk.cmake
+++ cmake/external_projects/tcltk.cmake
@@ -27,85 +27,35 @@ if(APPLE)
 
     set(TCL_DIR ${CMAKE_CURRENT_BINARY_DIR}/dependencies/src/project_tcl)
     set(TK_DIR ${CMAKE_CURRENT_BINARY_DIR}/dependencies/src/project_tk)
-    set(TCL_INCLUDE_PATH ${TCL_DIR}/generic;${TCL_DIR}/macosx)
-    set(TK_INCLUDE_PATH ${TK_DIR}/generic;${TK_DIR}/macosx;${TK_DIR}/xlib)
-    find_package(TCL 8.6 REQUIRED)
-    list(APPEND NETGEN_DEPENDENCIES project_tcl project_tk)
+    set(TCL_INCLUDE_PATH "${TCL_DIR}/generic;${TCL_DIR}/macosx")
+    set(TK_INCLUDE_PATH "${TK_DIR}/generic;${TK_DIR}/macosx;${TK_DIR}/xlib")
+    string(REPLACE ";" "$<SEMICOLON>" TCL_INC "${TCL_INCLUDE_PATH}")
+    string(REPLACE ";" "$<SEMICOLON>" TK_INC "${TK_INCLUDE_PATH}")
 
+    ExternalProject_Add(project_tkdnd
+      URL "http://sourceforge.net/projects/tkdnd/files/TkDND/TkDND%202.8/tkdnd2.8-src.tar.gz"
+      URL_MD5 a6d47a996ea957416469b12965d4db91
+      DEPENDS project_tcl project_tk
+      DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
+      PATCH_COMMAND  patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/tkdnd_macosx.patch
+      UPDATE_COMMAND "" # Disable update
+      BUILD_IN_SOURCE 1
+      CMAKE_ARGS
+	      -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/Contents/MacOS
+	      -DTCL_INCLUDE_PATH=${TCL_INC}
+	      -DTK_INCLUDE_PATH=${TK_INC}
+	      -DTK_LIBRARY=${TK_LIBRARY}
+	      -DTCL_LIBRARY=${TCL_LIBRARY}
+      LOG_DOWNLOAD 1
+      LOG_CONFIGURE 1
+      LOG_BUILD 1
+      LOG_INSTALL 1
+    )
+      
+list(APPEND NETGEN_DEPENDENCIES project_tkdnd)
   else()
     find_package(TCL 8.5 REQUIRED)
   endif()
-#   set(HOME $ENV{HOME})
-#   set(tcl_prefix ${CMAKE_INSTALL_PREFIX}/../../)
-#   ExternalProject_Add(project_tcl
-#     URL "http://sourceforge.net/projects/tcl/files/Tcl/8.6.4/tcl8.6.4-src.tar.gz"
-#     URL_MD5 d7cbb91f1ded1919370a30edd1534304
-#     DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
-#     UPDATE_COMMAND "" # Disable update
-#     CONFIGURE_COMMAND ../project_tcl/macosx/configure --enable-threads --enable-framework --prefix=${tcl_prefix} --libdir=${tcl_prefix}/Contents/Frameworks --bindir=${tcl_prefix}/Contents/Frameworks/Tcl.framework/bin
-#     BUILD_COMMAND make -j4 binaries libraries
-#     INSTALL_COMMAND make install-binaries install-headers install-libraries install-private-headers
-#     LOG_DOWNLOAD 1
-#     LOG_BUILD 1
-#     LOG_CONFIGURE 1
-#     LOG_INSTALL 1
-#     )
-# 
-#   ExternalProject_Add(project_tk
-#     DEPENDS project_tcl
-#     URL "http://sourceforge.net/projects/tcl/files/Tcl/8.6.4/tk8.6.4-src.tar.gz"
-#     URL_MD5 261754d7dc2a582f00e35547777e1fea
-#     DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
-#     UPDATE_COMMAND "" # Disable update
-#     CONFIGURE_COMMAND ../project_tk/macosx/configure --enable-aqua=yes --enable-threads --enable-framework --prefix=${tcl_prefix} --libdir=${tcl_prefix}/Contents/Frameworks --bindir=${tcl_prefix}/Contents/Frameworks/Tcl.framework/bin --with-tcl=${tcl_prefix}/Contents/Frameworks/Tcl.framework
-#     BUILD_COMMAND make -j4 binaries libraries
-#     INSTALL_COMMAND make install-binaries install-headers install-libraries install-private-headers
-#     LOG_DOWNLOAD 1
-#     LOG_BUILD 1
-#     LOG_CONFIGURE 1
-#     LOG_INSTALL 1
-#     )
-# 
-#   ExternalProject_Add(project_tkdnd
-#     URL "https://sourceforge.net/projects/tkdnd/files/OS%20X%20Binaries/TkDND%202.8/tkdnd2.8-OSX-MountainLion.tar.gz"
-#     URL_MD5 2dbb471b1d66c5f391f3c3c5b71548fb
-#     DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
-#     BUILD_IN_SOURCE 1
-#     CONFIGURE_COMMAND ""
-#     BUILD_COMMAND ""
-#     INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_directory . ${CMAKE_INSTALL_PREFIX}/../MacOS
-#     LOG_DOWNLOAD 1
-#     LOG_CONFIGURE 1
-#     LOG_BUILD 1
-#     LOG_INSTALL 1
-#     )
-#  
-#   list(APPEND NETGEN_DEPENDENCIES project_tcl project_tk project_tkdnd)
-#   list(APPEND CMAKE_PREFIX_PATH ${CMAKE_INSTALL_PREFIX}../Frameworks)
-#   set(TCL_INCLUDE_PATH ${CMAKE_INSTALL_PREFIX}/../Frameworks/Tcl.framework/Headers)
-#   set(TCL_LIBRARY ${CMAKE_INSTALL_PREFIX}/../Frameworks/Tcl.framework)
-#   set(TK_LIBRARY ${CMAKE_INSTALL_PREFIX}/../Frameworks/Tk.framework)
-#   set(TK_INCLUDE_PATH ${CMAKE_INSTALL_PREFIX}/../Frameworks/Tk.framework/Headers)
-# 
-
-
-
-ExternalProject_Add(project_tkdnd
-  URL "http://sourceforge.net/projects/tkdnd/files/TkDND/TkDND%202.8/tkdnd2.8-src.tar.gz"
-  URL_MD5 a6d47a996ea957416469b12965d4db91
-  DOWNLOAD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/external_dependencies
-  PATCH_COMMAND  patch -p1 < ${CMAKE_CURRENT_LIST_DIR}/tkdnd_macosx.patch
-  UPDATE_COMMAND "" # Disable update
-  BUILD_IN_SOURCE 1
-  CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}/Contents/MacOS --libdir=${CMAKE_INSTALL_PREFIX}/Contents/MacOS
-  BUILD_COMMAND make
-  INSTALL_COMMAND make install
-  LOG_DOWNLOAD 1
-  LOG_CONFIGURE 1
-  LOG_BUILD 1
-  LOG_INSTALL 1
-)
-list(APPEND NETGEN_DEPENDENCIES project_tkdnd)
 
 elseif(WIN32)
 
