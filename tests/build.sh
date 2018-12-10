3679ea7e45ed87cd3cda002e0e8bd1df8fc44fbf
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2018-12-10 10:57:56 +0100

    Enable core guidelines check, build on Ubuntu 18.04

--- tests/build.sh
+++ tests/build.sh
@@ -1,6 +1,6 @@
 cd
 mkdir -p build/netgen
 cd build/netgen
-cmake ../../src/netgen -DUSE_CCACHE=ON
+cmake ../../src/netgen -DUSE_CCACHE=ON -DENABLE_CPP_CORE_GUIDELINES_CHECK=ON
 make -j12
 make install
