ead955cf040a9f01743b6c4fd0f8c809f1eedf4b
Joachim Schöberl <joachim.schoeberl@tuwien.ac.at>
2019-02-07 20:32:01 +0100

    ngsimd namespace

--- libsrc/visualization/soldata.hpp
+++ libsrc/visualization/soldata.hpp
@@ -103,10 +103,10 @@ namespace netgen
 
 #ifdef __SSE__
     virtual bool GetMultiSurfValue (size_t selnr, size_t facetnr, size_t npts,
-                                    const netgen::tAVXd * xref, 
-                                    const netgen::tAVXd * x, 
-                                    const netgen::tAVXd * dxdxref, 
-                                    netgen::tAVXd * values)
+                                    const ngsimd::tAVXd * xref, 
+                                    const ngsimd::tAVXd * x, 
+                                    const ngsimd::tAVXd * dxdxref, 
+                                    ngsimd::tAVXd * values)
     {
       cerr << "GetMultiSurfVaue not overloaded for SIMD<double>" << endl;
       return false;
