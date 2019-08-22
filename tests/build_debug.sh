ff3e88e024c4bc44e979c865b30cc40565644dc0
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2019-08-22 10:17:57 +0200

    enable catch unit tests

--- tests/build_debug.sh
+++ tests/build_debug.sh
@@ -1,6 +1,6 @@
 cd
 mkdir -p build/netgen
 cd build/netgen
-cmake ../../src/netgen -DUSE_CCACHE=ON -DBUILD_TYPE=DEBUG
+cmake ../../src/netgen -DUSE_CCACHE=ON -DBUILD_TYPE=DEBUG -DENABLE_UNIT_TESTS=ON
 make -j12
 make install
