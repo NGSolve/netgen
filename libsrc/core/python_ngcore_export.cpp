b27f7f3bb6078145918714a2b8843b743deb6bbd
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-08-28 14:10:09 +0200

    Add BitArray from NGSolve
    Deprecate method BitArray::Set(), instead use either SetBit() or
    SetBitAtomic()

--- libsrc/core/python_ngcore_export.cpp
+++ libsrc/core/python_ngcore_export.cpp
@@ -1,5 +1,6 @@
-
 #include "python_ngcore.hpp"
+#include "bitarray.hpp"
+
 
 using namespace ngcore;
 using namespace std;
@@ -10,6 +11,83 @@ PYBIND11_MODULE(pyngcore, m) // NOLINT
   ExportArray<unsigned>(m);
   ExportArray<size_t>(m);
   ExportArray<double>(m);
+
+  py::class_<BitArray, shared_ptr<BitArray>> (m, "BitArray")
+    .def(py::init([] (size_t n) { return make_shared<BitArray>(n); }),py::arg("n"))
+    .def(py::init([] (const BitArray& a) { return make_shared<BitArray>(a); } ), py::arg("ba"))
+    .def(py::init([] (const vector<bool> & a)
+                  {
+                    auto ba = make_shared<BitArray>(a.size());
+                    ba->Clear();
+                    for (size_t i = 0; i < a.size(); i++)
+                      if (a[i]) ba->SetBit(i);
+                    return ba;
+                  } ), py::arg("vec"))
+    .def("__str__", &ToString<BitArray>)
+    .def("__len__", &BitArray::Size)
+    .def("__getitem__", [] (BitArray & self, int i)
+                                         {
+                                           if (i < 0 || i >= self.Size())
+                                             throw py::index_error();
+                                           return self.Test(i);
+                                         }, py::arg("pos"), "Returns bit from given position")
+    .def("__setitem__", [] (BitArray & self, int i, bool b)
+                                         {
+                                           if (i < 0 || i >= self.Size())
+                                             throw py::index_error();
+                                           if (b) self.SetBit(i); else self.Clear(i);
+                                         }, py::arg("pos"), py::arg("value"), "Clear/Set bit at given position")
+
+    .def("__setitem__", [] (BitArray & self, py::slice inds, bool b)
+                                         {
+                                           size_t start, step, stop, n;
+                                           if (!inds.compute(self.Size(), &start, &stop, &step, &n))
+                                             throw py::error_already_set();
+
+                                           if (start == 0 && n == self.Size() && step == 1)
+                                             { // base branch
+                                               if (b)
+                                                 self.Set();
+                                               else
+                                                 self.Clear();
+                                             }
+                                           else
+                                             {
+                                               if (b)
+                                                 for (size_t i=0; i<n; i++, start+=step)
+                                                   self.SetBit(start);
+                                               else
+                                                 for (size_t i=0; i<n; i++, start+=step)
+                                                   self.Clear(start);
+                                             }
+                                         }, py::arg("inds"), py::arg("value"), "Clear/Set bit at given positions")
+
+    .def("__setitem__", [](BitArray & self,  IntRange range, bool b)
+      {
+        if (b)
+          for (size_t i : range)
+            self.SetBit(i);
+        else
+          for (size_t i : range)
+            self.Clear(i);
+      }, py::arg("range"), py::arg("value"), "Set value for range of indices" )
+
+    .def("NumSet", &BitArray::NumSet)
+    .def("Set", [] (BitArray & self) { self.Set(); }, "Set all bits")
+    .def("Set", &BitArray::SetBit, py::arg("i"), "Set bit at given position")
+    .def("Clear", [] (BitArray & self) { self.Clear(); }, "Clear all bits")
+    .def("Clear", [] (BitArray & self, int i)
+                                   {
+                                       self.Clear(i);
+                                   }, py::arg("i"), "Clear bit at given position")
+
+
+    .def(py::self | py::self)
+    .def(py::self & py::self)
+    .def(py::self |= py::self)
+    .def(py::self &= py::self)
+    .def(~py::self)
+    ;
 
   py::class_<Flags>(m, "Flags")
     .def(py::init<>())
