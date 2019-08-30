6c06f79c8ef3aef6dda8b269ff634a4d3910bab8
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-08-29 19:22:12 -0700

    Fix build error on Windows (wrong usage of Range())

--- libsrc/core/table.hpp
+++ libsrc/core/table.hpp
@@ -120,7 +120,7 @@ public:
   NETGEN_INLINE Table (size_t asize, size_t entrysize)
     : FlatTable<T>( asize, new size_t[asize+1], new T[asize*entrysize] )
   {
-    for (size_t i : Range(size))
+    for (size_t i : IntRange(size))
       index[i] = i*entrysize;
   }
 
