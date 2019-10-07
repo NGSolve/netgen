18830a0a718dc3a9953842060b62d8481e4b0f91
Matthias Hochsteger <matthias.hochsteger@tuwien.ac.at>
2019-10-07 09:45:08 +0000

    Check for number of 1d and 2d elements
    Store test results in .json

--- tests/pytest/test_tutorials.py
+++ tests/pytest/test_tutorials.py
@@ -4,39 +4,42 @@ from netgen.meshing import meshsize, MeshingParameters, SetMessageImportance
 import netgen.csg as csg
 import netgen.stl as stl
 from pyngcore import TaskManager
+import json
 try:
     import netgen.occ as occ
     has_occ = True
 except ImportError:
     has_occ = False
-from results import *
 
 SetMessageImportance(0)
+
+def getData(mesh, mp):
+    out = {}
+    out['ne1d'] = len(mesh.Elements1D())
+    out['ne2d'] = len(mesh.Elements2D())
+    out['ne3d'] = len(mesh.Elements3D())
+    # round badness to avoid fluctuations in last digits
+    out["total_badness"] = float("{:.11g}".format(mesh.CalcTotalBadness(mp)))
+    out["quality_histogram"] = str(list(mesh.GetQualityHistogram()))
+    return out
+
+def checkData(mesh, mp, ref):
+    data = getData(mesh, mp)
+    assert ref['ne1d'] == data['ne1d']
+    assert ref['ne2d'] == data['ne2d']
+    assert ref['ne3d'] == data['ne3d']
+    assert ref['quality_histogram'] == data['quality_histogram']
+    assert ref['total_badness'] == pytest.approx(data['total_badness'], rel=1e-5)
+
 
 def getFiles(fileEnding):
     r, d, files = next(os.walk(os.path.join("..","..","tutorials")))
     return (f for f in files if f.endswith(fileEnding))
 
+@pytest.fixture
+def refdata():
+    return json.load(open('results.json','r'))
 
-def getCheckFunc(filename):
-    def func(mesh,mp,i):
-        if filename in number_elements:
-            # number of elements should be in 2% range of expected value
-            assert mesh.ne == number_elements[filename][i]
-            badness = mesh.CalcTotalBadness(mp)
-            qual_classes = list(mesh.GetQualityHistogram())
-            assert badness == pytest.approx(total_badness[filename][i], rel=1e-6)
-            assert qual_classes == quality_histogram[filename][i]
-    return func
-            
-def getResultFunc(filename):
-    def resultFunc(mesh, mp):
-        results = {}
-        results["number_elements"] = mesh.ne
-        results["total_badness"] = mesh.CalcTotalBadness(mp)
-        results["quality_histogram"] = list(mesh.GetQualityHistogram())
-        return results
-    return resultFunc
 
 def getMeshingparameters(filename):
     standard = [MeshingParameters()] + [MeshingParameters(ms)  for ms in (meshsize.very_coarse, meshsize.coarse, meshsize.moderate, meshsize.fine, meshsize.very_fine)]
@@ -55,7 +58,6 @@ def getMeshingparameters(filename):
 _geofiles = [f for f in getFiles(".geo")] + [f for f in getFiles(".stl")]
 if has_occ:
     _geofiles += [f for f in getFiles(".step")]
-
 _geofiles.sort()
 
 def generateMesh(filename, mp):
@@ -73,46 +75,51 @@ def isSlowTest(filename):
                         "period.geo", "shaft.geo", "cubeandring.geo", "ellipticcyl.geo",
                         "ellipsoid.geo", "cone.geo"]
 
-def getParamForTest(filename):
-    return pytest.param(filename, getCheckFunc(filename), marks=pytest.mark.slow) if isSlowTest(filename) \
-        else (filename, getCheckFunc(filename))
+def getParameters():
+    res = []
+    for f in _geofiles:
+        for i,mp in enumerate(getMeshingparameters(f)):
+            if isSlowTest(f):
+                res.append( pytest.param(f, mp, i, marks=pytest.mark.slow ) )
+            else:
+                res.append( (f, mp, i) )
+    return res
 
-@pytest.mark.parametrize(("filename, checkFunc"), [getParamForTest(f) for f in _geofiles])
-def test_geoFiles(filename, checkFunc):
+@pytest.mark.parametrize(("filename", "mp", "i"), getParameters())
+def test_geoFiles(filename, mp, i, refdata):
+    ref = refdata[filename]
     import filecmp
-    for i, mp_ in enumerate(getMeshingparameters(filename)):
-        print("load geo", filename)
-        mp = MeshingParameters(mp_, parallel_meshing=False)
-        mesh = generateMesh(filename, mp)
-        if checkFunc is not None:
-            checkFunc(mesh,mp,i)
-        mesh.Save(filename+'_seq.vol.gz')
+    print("load geo", filename)
+    mp = MeshingParameters(mp, parallel_meshing=False)
+    mesh = generateMesh(filename, mp)
+    mesh.Save(filename+'_seq.vol.gz')
+    with TaskManager():
+        mesh_par = generateMesh(filename, mp)
+        mesh_par.Save(filename+'_par.vol.gz')
 
-        with TaskManager():
-            mesh_par = generateMesh(filename, mp)
-            mesh_par.Save(filename+'_par.vol.gz')
+    assert filecmp.cmp(filename+'_seq.vol.gz', filename+'_par.vol.gz')
+    checkData(mesh, mp, ref[i])
 
-        assert filecmp.cmp(filename+'_seq.vol.gz', filename+'_par.vol.gz')
 
-import time
 def generateResultFile():
-  with TaskManager():
-    with open("results.py", "w") as f:
-        print("number_elements = {}", file=f)
-        print("total_badness = {}", file=f)
-        print("quality_histogram = {}", file=f)
-        for _file, _func in ((gf, getResultFunc(gf)) for gf in _geofiles):
+    import re, time
+    data = {}
+    with TaskManager():
+        for _file in _geofiles:
+            print("generate "+_file)
             start = time.time()
-            print("write", _file)
             mps = getMeshingparameters(_file)
             if not mps:
                 continue
-            results = [_func(generateMesh(_file, mp), mp) for mp in mps]
-            print("number_elements['{}'] = {}".format(_file, "(" + ",".join((str(r["number_elements"]) for r in results)) + ")"), file=f)
-            print("total_badness['{}'] = {}".format(_file, "(" + ",".join((str(r["total_badness"]) for r in results)) + ")"), file=f)
-            print("quality_histogram['{}'] = {}".format(_file, "(" + ",".join((str(r["quality_histogram"]) for r in results)) + ")"), file=f)
-            print("needed", time.time() - start, "seconds")
+            meshdata = []
+            for mp in mps:
+                mesh = generateMesh(_file, mp)
+                meshdata.append( getData(mesh, mp) )
+            data[_file] = meshdata
         
+        print("needed", time.time() - start, "seconds")
+    s = json.dumps(data, sort_keys=True, indent=4)
+    open("results.json", "w").write(s)
 
 if __name__ == "__main__":
     generateResultFile()
