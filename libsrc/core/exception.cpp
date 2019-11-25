de1932227b9a2543eb72703f885a40dd41e164f3
Christopher Lackner <clackner@cerbsim.com>
2019-11-25 16:45:12 +0100

    exception constructor outside of ifdef GNUC

--- libsrc/core/exception.cpp
+++ libsrc/core/exception.cpp
@@ -1,6 +1,28 @@
 #include "exception.hpp"
 #include "utils.hpp"
 
+namespace ngcore
+{
+  Exception :: Exception(const std::string& s)
+    : m_what(s) {}
+  
+  Exception :: Exception(const char* s)
+    : m_what(s) {}
+
+
+  void ThrowException(const std::string & s)
+  {
+    throw Exception (s);
+  }
+  
+  void ThrowException(const char * s)
+  {
+    throw Exception (s);
+  }
+} // namespace ngcore
+
+
+// ********* STUFF FOR GETBACKTRACE ***************************
 #ifdef __GNUC__
 
 #include <execinfo.h>
@@ -18,26 +40,6 @@
 
 namespace ngcore
 {
-
-
-  Exception :: Exception(const std::string& s)
-    : m_what(s) {}
-  
-  Exception :: Exception(const char* s)
-    : m_what(s) {}
-
-
-  void ThrowException(const std::string & s)
-  {
-    throw Exception (s);
-  }
-  
-  void ThrowException(const char * s)
-  {
-    throw Exception (s);
-  }
-  
-  
   namespace detail
   {
     static int exec(std::string cmd, std::string & out) {
