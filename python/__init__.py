97d3c1ce9e49cd611f08fc02000dff1eaa73cef2
Monty Montgomery <xiphmont@gmail.com>
2022-05-15 00:29:37 -0400

    Adjust python module path in __init__, add checks to pytest
    Adjust path setup of the python modules in __init__ such that it's
    possible to run pytests from a mock root during package build/test.
    
    Also add checks to pytest/CMakelists.txt to make sure pytest abd check
    are present instead of failing with an inscrutible traceback if
    they're not installed or usable.

--- python/__init__.py
+++ python/__init__.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 from . import config
-_netgen_bin_dir=os.path.realpath(os.path.join(os.path.dirname(__file__),'..',config.NETGEN_PYTHON_RPATH_BIN))
-_netgen_lib_dir=os.path.realpath(os.path.join(os.path.dirname(__file__),'..',config.NETGEN_PYTHON_RPATH))
+_netgen_bin_dir=os.path.realpath(os.path.join(config.NG_INSTALL_DIR_PYTHON,config.NETGEN_PYTHON_RPATH_BIN))
+_netgen_lib_dir=os.path.realpath(os.path.join(config.NG_INSTALL_DIR_PYTHON,config.NETGEN_PYTHON_RPATH))
 
 if sys.platform.startswith('win'):
     if sys.version >= '3.8':
