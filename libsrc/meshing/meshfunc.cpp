4ab01fa26983815cd3c232212cc24f90d2eda10f
Matthias Hochsteger <mhochsteger@cerbsim.com>
2023-04-21 12:52:17 +0200

    Save one FindOpenElements call in MeshVolume

--- libsrc/meshing/meshfunc.cpp
+++ libsrc/meshing/meshfunc.cpp
@@ -395,6 +395,7 @@ namespace netgen
 
       PrintMessage (3, mesh.GetNP(), " points, ",
          mesh.GetNE(), " elements");
+      mesh.FindOpenElements(domain);
    }
 
    Box<3> domain_bbox( Box<3>::EMPTY_BOX ); 
@@ -407,8 +408,6 @@ namespace netgen
          domain_bbox.Add (mesh[pi]);
      }
    domain_bbox.Increase (0.01 * domain_bbox.Diam());
-
-   mesh.FindOpenElements(domain);
 
    int cntsteps = 0;
    int meshed;
