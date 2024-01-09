fda7cfa2bc0dd2423fc2beae20b7458edaa64449
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-01-09 10:01:11 +0100

    function-pointer instead of std::function, no unique-ptr around type_register

--- libsrc/core/archive.cpp
+++ libsrc/core/archive.cpp
@@ -10,24 +10,24 @@
 namespace ngcore
 {
   // clang-tidy should ignore this static object
-  static std::unique_ptr<std::map<std::string, detail::ClassArchiveInfo>> type_register;  // NOLINT
+  static std::map<std::string, detail::ClassArchiveInfo> type_register;  // NOLINT
   const detail::ClassArchiveInfo& Archive :: GetArchiveRegister(const std::string& classname)
   {
-    if(type_register == nullptr) type_register =
-                                   std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
-    return (*type_register)[classname];
+    // if(type_register == nullptr) type_register =
+    // std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
+    return (type_register)[classname];
   }
   void Archive :: SetArchiveRegister(const std::string& classname, const detail::ClassArchiveInfo& info)
   {
-    if(type_register == nullptr) type_register =
-                                   std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
-    (*type_register)[classname] = info;
+    // if(type_register == nullptr) type_register =
+    // std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
+    (type_register)[classname] = info;
   }
   bool Archive :: IsRegistered(const std::string& classname)
   {
-    if(type_register == nullptr) type_register =
-                                   std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
-    return type_register->count(classname) != 0;
+    // if(type_register == nullptr) type_register =
+    // std::make_unique<std::map<std::string, detail::ClassArchiveInfo>>();
+    return type_register.count(classname) != 0;
   }
 
 #ifdef NETGEN_PYTHON
