e8a9131b3128fdcd1e12577f112ecdd2d1cd74b0
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2024-02-10 10:09:29 +0100

    include win first

--- libsrc/visualization/visual.hpp
+++ libsrc/visualization/visual.hpp
@@ -17,24 +17,6 @@ Visualization
 // #define PARALLELGL
 // #endif
 
-/*** Windows headers ***/
-#ifdef _MSC_VER
-# define WIN32_LEAN_AND_MEAN
-# ifndef NO_PARALLEL_THREADS
-#  ifdef MSVC_EXPRESS
-#  else
-#   include <afxwin.h>
-#   include <afxmt.h>
-#  endif // MSVC_EXPRESS
-# endif
-# include <windows.h>
-# undef WIN32_LEAN_AND_MEAN
-# include <winnt.h>
-
-#else // Not using MC VC++
-
-
-#endif
 
 
 
