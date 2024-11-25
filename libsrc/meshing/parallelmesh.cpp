44611e668c0c8fc316db7a65538b766e87043627
Matthias Hochsteger <mhochsteger@cerbsim.com>
2024-11-25 13:56:31 +0100

    Fix Segfault in MPI ReceiveMesh
    See https://github.com/NGSolve/netgen/issues/196

--- libsrc/meshing/parallelmesh.cpp
+++ libsrc/meshing/parallelmesh.cpp
@@ -1204,7 +1204,7 @@ namespace netgen
     auto write_names = [&] (auto & array) {
       for (int k = 0; k < array.Size(); k++) {
 	int s = name_sizes[tot_nn];
-	array[k] = new string(&compiled_names[tot_size], s);
+	array[k] = s ? new string(&compiled_names[tot_size], s) : nullptr;
 	tot_nn++;
 	tot_size += s;
       }
