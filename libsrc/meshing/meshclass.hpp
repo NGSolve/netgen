643898c5e28de62a77637169b9ee9877c97999f2
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-01-02 20:51:11 +0100

    avoid shared ptr copy

--- libsrc/meshing/meshclass.hpp
+++ libsrc/meshing/meshclass.hpp
@@ -507,7 +507,7 @@ namespace netgen
     ///
     LocalH & LocalHFunction (int layer=1) { return * lochfunc[layer-1]; }
 
-    shared_ptr<LocalH> GetLocalH(int layer=1) const
+    shared_ptr<LocalH> & GetLocalH(int layer=1) const
     {
       if(lochfunc.Size() == 1)
         return lochfunc[0];
