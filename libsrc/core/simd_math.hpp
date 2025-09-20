1ec9007e05dafd3e6a4a665b03300f6618c08931
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-09-20 08:20:11 +0200

    avoid div in simd-sincos

--- libsrc/core/simd_math.hpp
+++ libsrc/core/simd_math.hpp
@@ -57,7 +57,7 @@ namespace ngcore
   template <int N>
   auto sincos (SIMD<double,N> x)
   {
-    auto y = round(x / (M_PI/2));
+    auto y = round((2/M_PI) * x);
     auto q = lround(y);
   
     auto [s1,c1] = sincos_reduced(x - y * (M_PI/2));
