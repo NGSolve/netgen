cd974c3b301b7355900c76dcc30b63545bed4b9f
Matthias Hochsteger <mhochsteger@cerbsim.com>
2025-10-17 12:04:45 +0200

    Move ShapeProperties::DoArchive from .hpp to .cpp

--- libsrc/meshing/basegeom.hpp
+++ libsrc/meshing/basegeom.hpp
@@ -42,12 +42,7 @@ namespace netgen
     string GetName() const { return name ? *name : "default"; }
     Vec<4> GetColor() { return col ? *col : Vec<4>{0., 1., 0., 1.}; }
 
-    void DoArchive(Archive& ar)
-    {
-        ar & name & col & maxh & hpref & layer;
-        if(ar.GetVersion("netgen") > "v6.2.2506-14")
-            ar & partition & quad_dominated;
-    }
+    void DoArchive(Archive& ar);
   };
 
   class GeometryShape;
