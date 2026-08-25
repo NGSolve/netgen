14c56ed182018320670bb2a996a092037c79c571
erdieee <58039191+erdieee@users.noreply.github.com>
2026-08-25 10:24:03 +0200

    feat: Add SIMD<float,2> ARM specialization

--- libsrc/core/simd_arm64.hpp
+++ libsrc/core/simd_arm64.hpp
@@ -177,6 +177,95 @@ namespace ngcore
   // *************************** float ***************************
 
   template<>
+  class SIMD<float,2>
+  {
+    float32x2_t data;
+
+  public:
+    static constexpr int Size() { return 2; }
+    SIMD () {}
+    SIMD (const SIMD &) = default;
+    SIMD (float v0, float v1) : data{v0,v1} { }
+    SIMD (SIMD<float,1> v0, SIMD<float,1> v1) : data{v0.Data(),v1.Data()} { }
+    SIMD (std::array<float, 2> arr) : data{arr[0],arr[1]} { }
+
+    SIMD & operator= (const SIMD &) = default;
+
+    SIMD (float val)  : data{val,val} { }
+    SIMD (double val) : SIMD(float(val)) { }
+    SIMD (int val)    : data{float(val),float(val)} { }
+    SIMD (size_t val) : data{float(val),float(val)} { }
+
+    SIMD (float const * p)
+    {
+      data = vld1_f32(p);
+    }
+
+    SIMD (float32x2_t _data) { data = _data; }
+
+    template<typename T, typename std::enable_if<std::is_convertible<T, std::function<float(int)>>::value, int>::type = 0>
+    SIMD (const T & func)
+    {
+      data[0] = func(0);
+      data[1] = func(1);
+    }
+
+    void Store (float * p)
+    {
+      vst1_f32(p, data);
+    }
+
+    NETGEN_INLINE float operator[] (int i) const { return data[i]; }
+    NETGEN_INLINE float & operator[] (int i) { return ((float*)&data)[i]; }
+
+    template <int I>
+    float Get() const { return data[I]; }
+
+    NETGEN_INLINE auto Data() const { return data; }
+    NETGEN_INLINE auto & Data() { return data; }
+
+    float Lo() const { return Get<0>(); }
+    float Hi() const { return Get<1>(); }
+  };
+
+
+  NETGEN_INLINE float HSum (SIMD<float,2> sd)
+  {
+    return vaddv_f32(sd.Data());
+  }
+
+  NETGEN_INLINE SIMD<float,2> FMA (SIMD<float,2> a, SIMD<float,2> b, SIMD<float,2> c)
+  {
+    return vmla_f32(c.Data(), a.Data(), b.Data());
+  }
+
+  NETGEN_INLINE SIMD<float,2> FMA (const float & a, SIMD<float,2> b, SIMD<float,2> c)
+  {
+    return FMA(SIMD<float,2> (a), b, c);
+  }
+
+  NETGEN_INLINE void FMAComplex (SIMD<float,2> a, SIMD<float,2> b, SIMD<float,2> & c)
+  {
+    auto tmp = vcmla_f32(c.Data(), a.Data(), b.Data());
+    c = vcmla_rot90_f32(tmp, a.Data(), b.Data());
+  }
+
+  NETGEN_INLINE SIMD<float,2> operator+ (SIMD<float,2> a, SIMD<float,2> b)
+  { return a.Data()+b.Data(); }
+
+  NETGEN_INLINE SIMD<float,2> operator- (SIMD<float,2> a, SIMD<float,2> b)
+  { return a.Data()-b.Data(); }
+  NETGEN_INLINE SIMD<float,2> operator- (SIMD<float,2> a)
+  { return -a.Data(); }
+
+  NETGEN_INLINE SIMD<float,2> operator* (SIMD<float,2> a, SIMD<float,2> b)
+  { return a.Data()*b.Data(); }
+
+  NETGEN_INLINE SIMD<float,2> operator/ (SIMD<float,2> a, SIMD<float,2> b)
+  { return a.Data()/b.Data(); }
+
+
+  template<>
   class SIMD<float,4>
   {
     float32x4_t data;
@@ -186,7 +275,7 @@ namespace ngcore
     SIMD () {}
     SIMD (const SIMD &) = default;
     SIMD (float v0, float v1, float v2, float v3) : data{v0,v1,v2,v3} { }
-    SIMD (SIMD<float,2> v0, SIMD<float,2> v1) : data{v0[0],v0[1],v1[0],v1[1]} { }
+    SIMD (SIMD<float,2> v0, SIMD<float,2> v1) : data{vcombine_f32(v0.Data(),v1.Data())} { }
     SIMD (std::array<float, 4> arr) : data{arr[0],arr[1],arr[2],arr[3]} { }
 
     SIMD & operator= (const SIMD &) = default;
@@ -226,8 +315,8 @@ namespace ngcore
     NETGEN_INLINE auto Data() const { return data; }
     NETGEN_INLINE auto & Data() { return data; }
 
-    SIMD<float,2> Lo() const { return SIMD<float,2> (data[0],data[1]); }
-    SIMD<float,2> Hi() const { return SIMD<float,2> (data[2],data[3]); }
+    SIMD<float,2> Lo() const { return vget_low_f32(data); }
+    SIMD<float,2> Hi() const { return vget_high_f32(data); }
   };
 
 
